eed50715ad7036e758407c15bfae242052e3dcbf
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2018-12-12 13:45:54 +0100

    Code added from Graph.h for problems

--- Exercises/Marp30.cpp
+++ Exercises/Marp30.cpp
@@ -34,3 +34,51 @@ int main()
 
   return 0;
 }
+
+//----------------------------------------------------------------------------//
+//--------------------------- MÉTODOS AÑADIDOS -------------------------------//
+//----------------------------------------------------------------------------//
+// Francisco Javier Blázquez Martínez ----------------------- frblazqu@ucm.es //
+/*
+int numElemsMayorCompConexa() const
+{
+  if(_V == 0)  return 0;
+
+  std::vector<bool> marcados(_V, false);   // Para marcar los vértices visitados
+  std::vector<int>  pendientes;            // Para guardar los vértices por explorar
+  int maxElemsComponente = 1, numElemsEstaComponente;
+
+  for(int i = 0; i < _V; i++)
+  {
+    if(!marcados[i])
+    {
+    numElemsEstaComponente = 1;
+
+    pendientes.push_back(i);
+    marcados[i] = true;
+
+    while(!pendientes.empty())
+    {
+      int vertex = pendientes.back(); pendientes.pop_back();
+
+      for(int j = 0; j < _ady[vertex].size(); j++)
+      {
+        int ady = _ady[vertex][j]; // ady es adyacente a vertex
+
+        // Si el vértice no está marcado, uno más de la componente conexa
+        if(!marcados[ady])
+        {
+          numElemsEstaComponente++;
+          pendientes.push_back(ady);
+          marcados[ady] = true;
+
+          if(numElemsEstaComponente > maxElemsComponente)
+             maxElemsComponente = numElemsEstaComponente;
+        }
+      }
+    }
+  }
+  }
+
+  return maxElemsComponente;
+}*/
--- Exercises/Marp31.cpp
+++ Exercises/Marp31.cpp
@@ -37,3 +37,53 @@ int main()
 
   return 0;
 }
+
+//----------------------------------------------------------------------------//
+//--------------------------- MÉTODOS AÑADIDOS -------------------------------//
+//----------------------------------------------------------------------------//
+// Francisco Javier Blázquez Martínez ----------------------- frblazqu@ucm.es //
+/*
+bool esBipartito() const
+{
+  if(_V <= 1)  return true;
+
+  bool bipartito = true, oposite;
+  int vertex, adyacente;
+
+  bool marcado[_V];    for(int i = 0; i < _V; i++) marcado[i]   = false;
+  bool rojoNegro[_V];  for(int i = 0; i < _V; i++) rojoNegro[i] = false;
+  std::vector<int> pendientes;
+
+  for(int i = 0; i < _V && bipartito; i++)
+  {
+    if(!marcado[i])
+    {
+      pendientes.push_back(i);
+      marcado[i] = true;      // Lo marcamos como visitado
+      rojoNegro[i] = true;    // El primer vértice de la componente conexa es rojo (true)
+
+      while(!pendientes.empty() && bipartito)
+      {
+        vertex   = pendientes.back(); pendientes.pop_back(); // Tomamos el vértice
+        oposite  = (rojoNegro[vertex]? false : true);        // Color distinto para los adyacentes
+
+        for(int j=0; j<_ady[vertex].size(); j++)
+        {
+          adyacente = _ady[vertex][j];             // Tomamos el vértice adyacente
+
+          if(!marcado[adyacente])
+          {
+            marcado[adyacente]   = true;
+            rojoNegro[adyacente] = oposite;
+            pendientes.push_back(adyacente);
+          }
+          else bipartito &= (rojoNegro[adyacente] == oposite);
+        }
+      }
+
+    }
+  }
+
+  return bipartito;
+}
+*/
